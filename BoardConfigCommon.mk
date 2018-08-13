370b1aa4370207f2a7a9bbff1907f7990a172719
Jviix <jviix8@gmail.com>
2018-08-13 12:10:19 +0200

    PE-ify

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -39,10 +39,9 @@ TARGET_HAS_LEGACY_CAMERA_HAL1 := true
 BOARD_BATTERY_DEVICE_NAME := "battery"
 BOARD_CHARGING_CMDLINE_NAME := "androidboot.bootchg"
 BOARD_CHARGING_CMDLINE_VALUE := "true"
-WITH_LINEAGE_CHARGER := false
 
 # CMHW
-BOARD_HARDWARE_CLASS += device/samsung/msm8974-common/lineagehw
+BOARD_HARDWARE_CLASS += device/samsung/msm8974-common/samsunghw
 
 # Dexpreopt
 ifeq ($(HOST_OS),linux)
